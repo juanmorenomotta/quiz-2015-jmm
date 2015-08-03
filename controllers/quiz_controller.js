32b703bebaee991749a37647ef0da78b32d8b63d
Juan Moreno <juanmorenomotta@gmail.com>
2015-08-03 12:04:35 -0500

    Formulario de busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,9 +18,10 @@ exports.load = function(req, res, next, quizId){
 
 // GET /quizes
 exports.index = function(req, res){
-	models.Quiz.findAll().then(
+	var search = '%'+(req.query.search||"").replace(/ /,'%')+'%';
+	models.Quiz.findAll({where: ["pregunta like ?", search]}).then(
 		function(quizes){
-			res.render('quizes/index', { quizes: quizes});
+			res.render('quizes/index', { quizes: quizes });
 		}
 	).catch(function(error){ next(error); });
 };
