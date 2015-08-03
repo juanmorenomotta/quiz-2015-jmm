edca7d28f4f8807801fba83088ea0220e6402da0
Juan Moreno <juanmorenomotta@gmail.com>
2015-08-02 21:37:38 -0500

    Base de datos SQLite y Postgres

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/models.js');
 
 // GET quizes/question
 exports.question = function(req, res){
-  res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().success(function(quiz){
+		res.render('quizes/question',{pregunta: quiz[0].pregunta});
+	});
 };
 
+// GET /quizes/answer
+exports.answer = function(req, res){
+	models.Quiz.findAll().success(function(quiz){
+		if (req.query.respuesta === quiz[0].respuesta){
+			res.render('quizes/answer', {respuesta: 'Correcto'});
+		} else {
+			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		}
+	});
+};
+
+/*
 exports.answer = function(req, res){
   if (req.query.respuesta === 'Roma'){
     res.render('quizes/answer', {respuesta: 'Correcto'});
@@ -13,3 +28,4 @@ exports.answer = function(req, res){
     res.render('quizes/answer', {respuesta: 'Incorrecto'});
   }
 };
+*/
